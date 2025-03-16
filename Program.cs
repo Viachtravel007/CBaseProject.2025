d2b26a5736d788ae66b2155c10199de58551978a
Viacheslav <viachtravel007@gmail.com>
2025-03-16 20:00:03 +0200

    exactly created a demo program (problem with the previous commit)

--- Program.cs
+++ Program.cs
@@ -2,9 +2,25 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            string[] rainbowColors = { "Red", "Orange", "Yellow", "Green", "Blue", "Cyan", "Magenta" };
+
+            ConsoleColor[] colors = {
+            ConsoleColor.Red,
+            ConsoleColor.DarkYellow,
+            ConsoleColor.Yellow,
+            ConsoleColor.Green,
+            ConsoleColor.Blue,
+            ConsoleColor.Cyan,
+            ConsoleColor.Magenta
+            };
+
+            for (int i = 0; i < rainbowColors.Length; i++)
+            {
+                Console.ForegroundColor = colors[i];
+                Console.WriteLine(rainbowColors[i]);
+            }
         }
     }
 }
