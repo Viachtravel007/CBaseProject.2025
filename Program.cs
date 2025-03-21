1fcb4a6198652b4aeb3d4f732b7487987cbc3ff4
Viacheslav <viachtravel007@gmail.com>
2025-03-21 22:22:33 +0200

    added random events, and removed unnecessary code

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System;
 
 namespace DefaultProject
 {
@@ -38,7 +37,58 @@ namespace DefaultProject
                         Console.WriteLine($"Result: {number1 / number2}");
                     else
                     {
-                        Console.WriteLine("Error: Division by zero");
+                        Random random = new Random();
+                        int events = random.Next(1, 4);
+                        switch (events) 
+                        {
+                            case 1:
+                                string[] lines = {
+                            "C:\\Windows\\System32> del *.*",
+                            "",
+                            "C:\\Windows\\System32> Deleting file: kernel32.dll",
+                            "C:\\Windows\\System32> Deleting file: user32.dll",
+                            "C:\\Windows\\System32> Deleting file: ntdll.dll",
+                            "C:\\Windows\\System32> Deleting file: advapi32.dll",
+                            "C:\\Windows\\System32> Deleting file: gdi32.dll",
+                            "C:\\Windows\\System32> Deleting file: shell32.dll",
+                            "C:\\Windows\\System32> Deleting file: cmd.exe",
+                            "C:\\Windows\\System32> Deleting file: winlogon.exe",
+                            "C:\\Windows\\System32> Deleting file: services.exe",
+                            "C:\\Windows\\System32> Deleting file: svchost.exe",
+                            "C:\\Windows\\System32> Deleting file: spoolsv.exe",
+                            "C:\\Windows\\System32> Deleting file: msconfig.exe",
+                            "C:\\Windows\\System32> Deleting file: regedit.exe",
+                            "C:\\Windows\\System32> Deleting file: lsass.exe",
+                            "C:\\Windows\\System32> Deleting file: taskmgr.exe",
+                            "C:\\Windows\\System32> Deleting file: explorer.exe",
+                            "C:\\Windows\\System32> Deleting file: wininit.exe",
+                            "",
+                            "C:\\Windows\\System32> All files deleted successfully.",
+                            "",
+                            "C:\\Windows\\System32> The system will now shut down to prevent further damage."
+                            };
+
+                                foreach (string line in lines)
+                                {
+                                    Console.WriteLine(line);
+                                    Thread.Sleep(random.Next(100, 666));
+                                }
+                                break;
+
+                            case 2:
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Divide by zero? The smartest one?");
+                                Console.ResetColor();
+                                break;
+
+                            case 3:
+
+                                Console.WriteLine($"Your data has been transmitted to the SBU. " +
+                                    $"\nYour detention will be handled by the 107th Reactive Artillery Brigade of Kremenchuk. " +
+                                    $"\nPlease step closer to the window");
+                                break;
+                        }
+                        
                     }
                     break;
 
@@ -47,7 +97,6 @@ namespace DefaultProject
                     number1 = double.Parse(Console.ReadLine());
                     Console.Write("Input exponent: ");
                     number2 = double.Parse(Console.ReadLine());
-                    result = Math.Pow(number1, number2);
                     Console.WriteLine($"Result: {Math.Pow(number1, number2)}");
                     break;
 
