328bc6f3ead45167706b6f6595166bd8ede41dff
Viacheslav <viachtravel007@gmail.com>
2025-04-12 21:21:02 +0300

    add creation a random array and actions with it

--- ArrayProgram.cs
+++ ArrayProgram.cs
@@ -11,6 +11,38 @@ namespace DefaultProject
         public static void ArrayEnter()
         {
 
+            Random random = new Random();
+
+            int[] randomArray = new int[10];
+
+            for (int i = 0; i < randomArray.Length; i++)
+            {
+                randomArray[i] = random.Next(-10, 11);
+                if (i % 2 == 0)
+                {
+                    Console.Write(randomArray[i] + " ");
+                }
+            }
+            Console.WriteLine();
+
+            int sum = 0;
+            foreach (int num in randomArray)
+            {
+                sum = sum + num;
+            }
+            if (sum > 0)
+            {
+                Console.WriteLine("array sum more than 0");
+            } 
+            else if (sum < 0)
+            {
+                Console.WriteLine("array sum less than 0");
+            }
+            else
+            {
+                Console.WriteLine("Balance");
+            }
+
         }
     }
 }
