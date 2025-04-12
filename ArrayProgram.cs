5e5501e3166ecf58ec48b8c3d4f18f4a17d6e85c
Viacheslav <viachtravel007@gmail.com>
2025-04-12 22:09:27 +0300

    add array with a multiplication table and a toothed array

--- ArrayProgram.cs
+++ ArrayProgram.cs
@@ -33,7 +33,7 @@ namespace DefaultProject
             if (sum > 0)
             {
                 Console.WriteLine("array sum more than 0");
-            } 
+            }
             else if (sum < 0)
             {
                 Console.WriteLine("array sum less than 0");
@@ -42,7 +42,60 @@ namespace DefaultProject
             {
                 Console.WriteLine("Balance");
             }
+            Console.WriteLine("---------------------------------------");
 
+            int[,] multiArray = new int[10, 10];
+
+            for (int i = 0; i < multiArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < multiArray.GetLength(1); j++)
+                {
+                    multiArray[i, j] = (i + 1) * (j + 1);
+                    Console.Write($"{multiArray[i, j],3} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("---------------------------------------");
+
+            int[][] jaggernautArray = new int[5][];
+            jaggernautArray[0] = new int[5];
+            jaggernautArray[1] = new int[4];
+            jaggernautArray[2] = new int[5];
+            jaggernautArray[3] = new int[6];
+            jaggernautArray[4] = new int[5];
+
+            for (int i = 0; i < jaggernautArray.Length; i++)
+            {
+                for (int j = 0; j < jaggernautArray[i].Length; j++)
+                {
+                    jaggernautArray[i][j] = random.Next(-100, 101);
+                    Console.Write($"{jaggernautArray[i][j],4} ");
+                }
+                Console.WriteLine();
+            }
+
+            int minElement = 100;
+            int[] indexMin = { 0, 0 };
+            int maxElement = -100;
+            int[] indexMax = { 0, 0 };
+            for (int i = 0; i < jaggernautArray.Length; i++)
+            {
+                for (int j = 0; j < jaggernautArray[i].Length; j++)
+                {
+                    if (jaggernautArray[i][j] < minElement)
+                    {
+                        minElement = jaggernautArray[i][j];
+                        indexMin = [i , j];
+                    }
+                    if (jaggernautArray[i][j] > maxElement)
+                    {
+                        maxElement = jaggernautArray[i][j];
+                        indexMax = [i , j];
+                    }
+                }
+            }
+            Console.WriteLine($"{minElement} with index ({indexMin[0]}; {indexMin[1]})");
+            Console.WriteLine($"{maxElement} with index ({indexMax[0]}; {indexMax[1]})");
         }
     }
 }
